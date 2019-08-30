2b4fe36012af899751e94fcf632c3881e6ddb684
Frederic Auberson <frederic@auberson.net>
2019-08-30 23:47:14 +0200

    Pass startup script name when installing

--- src/main/scala/riot/riotctl/sbt/RiotCtl.scala
+++ src/main/scala/riot/riotctl/sbt/RiotCtl.scala
@@ -2,6 +2,7 @@ package riot.riotctl.sbt
 
 import sbt._
 import sbt.Keys.{ sLog, target }
+import sbt.plugins._
 import riot.riotctl.Target
 import riot.riotctl.RiotCtlTool
 import scala.collection.Seq
@@ -9,15 +10,20 @@ import scala.collection.JavaConverters
 import com.typesafe.sbt._
 import com.typesafe.sbt.packager.Keys._
 import sbt.internal.LogManager
-
+import com.typesafe.sbt.SbtNativePackager.{Debian, Linux, Universal}
+import com.typesafe.sbt.packager.linux.LinuxKeys
+import com.typesafe.sbt.packager.debian.DebianPlugin
+import com.typesafe.sbt.packager.archetypes.JavaServerAppPackaging
+import com.typesafe.sbt.packager.archetypes.systemloader.SystemdPlugin
+import com.typesafe.sbt.packager.debian.DebianKeys
 object RiotCtl extends AutoPlugin {
   override val trigger: PluginTrigger = allRequirements
-  override val requires: Plugins = plugins.JvmPlugin && packager.universal.UniversalPlugin && packager.archetypes.systemloader.SystemdPlugin
+  override val requires: Plugins = JvmPlugin && JavaServerAppPackaging && DebianPlugin && SystemdPlugin
 
   object Keys {
     lazy val riotPrereqs = settingKey[String]("Prerequisite apt-get packages, space-separated. Defaults to Java 8 and WiringPi.")
     lazy val riotTargets = taskKey[Seq[riotTarget]]("Address and access credentials of the target device (e.g. 'raspberrypi', 'pi', 'raspberry')")
-    
+
     lazy val install = taskKey[Unit]("Installs an application to a Linux SBC.")
   }
 
@@ -34,7 +40,7 @@ object RiotCtl extends AutoPlugin {
       log.error(s)
     }
   }
-  
+
   val autoImport = Keys
   import autoImport._
 
@@ -47,19 +53,23 @@ object RiotCtl extends AutoPlugin {
   private def installTask = Def.task {
     val log = sLog.value
     val pkgName = packageName.value
-    val pkg = new File(pkgName)
-    val dir = stage.value
-    
+    val stageDir = stage.value
+    val systemdConf = (linuxMakeStartScript in Debian).value
+
     val u = new RiotCtlTool(pkgName, JavaConverters.seqAsJavaList(riotTargets.value), new sbtLogger(log))
 
     u.ensurePackages(riotPrereqs.value)
-    
-    log.info(s"Deploying $pkgName from '$dir'")
-    u.install(pkg)
-    
+
+    systemdConf match {
+      case Some(confFile) =>
+        u.install(stageDir, confFile)
+      case None =>
+        log.info("No configuration file found, application was only copied but not installed")
+    }
+
     u.close();
-    
-    pkg
+
+    stageDir
   }
-  
+
 }
