77b9f2cf4c9e8f55cd26fa6c91bd90a51ca375b2
Frédéric Auberson <fauberso@users.noreply.github.com>
2019-09-28 14:15:52 +0200

    Delete release.sh

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export GPG_TTY=$(tty)
-echo HOWTO:
-echo "1. publishSigned (sonatypeDrop if it fails)"
-echo "2. review results in Sonatype's staging repository"
-echo "3. sonatypeReleaseAll"
-sbt
