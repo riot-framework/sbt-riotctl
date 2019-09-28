96c7ed35af134f03bfe6a7cc6aeef15a167f6c2e
Frederic Auberson <frederic@auberson.net>
2019-09-28 14:11:41 +0200

    Use default JDK for the current distribution, should in general be >8 these days.

--- src/main/scala/riot/riotctl/sbt/RiotCtl.scala
+++ src/main/scala/riot/riotctl/sbt/RiotCtl.scala
@@ -68,7 +68,7 @@ object RiotCtl extends AutoPlugin {
     riotRequiresSerial := false,
     riotRequiresOnewire := false,
     riotUpdateTime := true,
-    riotPrereqs := "openjdk-8-jdk wiringpi",
+    riotPrereqs := "default-jdk-headless wiringpi",
     riotDbgPort := 8000,
     riotInstall := installTask.value,
     riotUninstall := uninstallTask.value,
